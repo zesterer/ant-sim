d2d7f6822c3f24303afabceda09bc1a6845d85e7
Joshua Barretto <barry.of.smith@gmail.com>
2015-12-14 16:41:15 +0000

    Added initial project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# AntSim
+
+A C# university assignment designed to simulate the gathering behavior of ants and other social insects.
---
+++ anti-sim/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anti_sim
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
+
+            return 0;
+        }
+    }
+}
